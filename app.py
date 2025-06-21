f9072b0eddff076755e53aa7d9e94c181be81f1b
FabioLopez841 <fabiolopez.bluefish@gmail.com>
2025-06-21 15:52:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
 
 # Configuración de la página
 st.set_page_config(page_title="Dashboard de Productos", layout="wide")
